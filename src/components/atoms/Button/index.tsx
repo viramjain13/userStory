fc6414b141896de1f388001546f2f01824a96d83
viramjain13 <viram.jain@zemosolabs.com>
2021-11-17 15:33:45 +0530

    step1

---
+++ src/components/atoms/Button/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ButtonComponent = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/components/molecules/BookDetail/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const BookDetail = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}

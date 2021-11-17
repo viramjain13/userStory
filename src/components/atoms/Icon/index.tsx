8d4445adb9ed300ece48455594fb23d1074b2195
viramjain13 <viram.jain@zemosolabs.com>
2021-11-17 11:30:43 +0530

    adding props

--- src/components/atoms/Icon/index.tsx
+++ src/components/atoms/Icon/index.tsx
@@ -1,9 +1,17 @@
+import { Icon } from '@material-ui/core'
 import React from 'react'
 
-export const index = () => {
+export type IconProp={
+    image:string
+    styles?:Object
+}
+function index(props:IconProp) {
+    const { image, styles } = props
     return (
-        <div>
-            
-        </div>
+        <Icon   >
+            <img src={image} alt=""/>
+        </Icon>
     )
 }
+
+export default index
--- src/components/organisms/ExploreMenu/index.tsx
+++ src/components/organisms/ExploreMenu/index.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Tab, Tabs } from "@material-ui/core";
+import React from "react";
 
+type tabPanel = {
+  children: any;
+  value: any;
+  index: any;
+};
 export const index = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  function TabPanel(props: tabPanel) {
+    const { children, value, index } = props;
+    return <>{value === index && <h1>{children}</h1>}</>;
+  }
+
+  let exploreByCategory=<></>
+  let recentTitles=<></>
+  let mostPopularTitle=<></>
+  
+  return (
+    <div>
+      <Tabs>
+        <Tab></Tab>
+        <Tab></Tab>
+        <Tab></Tab>
+      </Tabs>
+      <TabPanel value={0} index={0}>{exploreByCategory}</TabPanel>
+      <TabPanel value={0} index={0}>{recentTitles}</TabPanel>
+      <TabPanel value={0} index={0}>{mostPopularTitle}</TabPanel>
+    </div>
+  );
+};
